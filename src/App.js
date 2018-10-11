5f24fa7b735b4adb6bbf89582b0cccd6b4e9f2db
RyderBlack <ryhad.boughanmi@gmail.com>
2018-10-10 21:42:11 -0400

    import apollo client to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient();
 
 class App extends Component {
   render() {
