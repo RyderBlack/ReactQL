88596826780cdd183b8e3bfddcc95483486a2978
RyderBlack <ryhad.boughanmi@gmail.com>
2018-10-10 21:49:17 -0400

    adding client and moving to index.js v01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ApolloClient from 'apollo-boost';
-
-const client = new ApolloClient();
 
 class App extends Component {
   render() {
